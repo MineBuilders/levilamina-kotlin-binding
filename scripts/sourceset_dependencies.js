322fa2eab49ce255d7912621969135c3e45b7e9b
Cdm2883 <Cdm2883@users.noreply.github.com>
2025-03-31 16:51:21 +0000

    deploy: 129322af41d7cdbd9c136e1e82e9f5b3873ec3d6

--- scripts/sourceset_dependencies.js
+++ scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":lse-reference/commonMain":[],":lse-reference/jsMain":[":lse-reference/commonMain"]}'
+sourceset_dependencies = '{":komomo-reference/commonMain":[],":komomo-reference/jsMain":[":komomo-reference/commonMain"],":lse-reference/commonMain":[],":lse-reference/jsMain":[":lse-reference/commonMain"]}'
